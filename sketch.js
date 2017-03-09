ecd0b576580a9b0222ce952b476b4c269cb343ef
sirrah23 <hseth93@gmail.com>
2017-03-08 22:23:53 -0500

    Created initial basic objects

---
+++ sketch.js
@@ -0,0 +1,112 @@
+var direction = 0;
+var player,
+	ball,
+	obstacle;
+
+function setup(){
+	createCanvas(800, 500);
+	player = new Player();
+	ball = new Ball(player);
+	obstacle = new Obstacle(width/2, height/2);
+}
+
+function draw(){
+	background(51);
+	player.move(direction);
+	ball.move();
+	player.show();
+	ball.show();
+	obstacle.show();
+}
+
+function Player(){
+	this.width = 200;
+	this.height=25;
+	this.x = width/3;
+	this.y = height-this.height*2;
+	this.step = 15;
+}
+
+Player.prototype.show = function(){
+	push();
+	fill(255);
+	rect(this.x, this.y, this.width, this.height);
+	pop();
+}
+
+Player.prototype.move = function(dir){
+	this.x += this.step*dir;
+	if (this.x < 0){
+		this.x = 0;
+	} else if (this.x + this.width > width){
+		this.x = width - this.width;
+	}
+}
+
+function Ball(){
+	this.width = 15;
+	this.height = 15;
+	this.x = 0 + this.width;
+	this.y = height - this.height;
+	this.vel = createVector(2,-1);
+	this.step = 5;
+}
+
+Ball.prototype.show = function(){
+	push();
+	fill(255);
+	rect(this.x, this.y, this.width, this.height);
+	pop();
+}
+
+Ball.prototype.move = function(){
+	this.x += this.vel.x * this.step;
+	this.y += this.vel.y * this.step;
+	// Function
+	if(this.x < 0){
+		this.x = 0;
+		this.vel.x *= -1;
+	} else if (this.x + this.width > width){
+		this.x = width - this.width;
+		this.vel.x *= -1;
+	}
+	// Function
+	if (this.y < 0){
+		this.y = 0;
+		this.vel.y *= -1; 
+	}
+}
+
+// TODO: Write this function once we have obstacles
+// Ball.prototype.collided = function(obtacle){
+// }
+
+function Obstacle(x, y){
+	this.x = x;
+	this.y = y;
+	this.width = 200;
+	this.height = 25;
+	this.col = createVector(random(255), random(255), random(255)); 
+}
+
+Obstacle.prototype.show = function(){
+	push();
+	fill(this.col.x, this.col.y, this.col.z);
+	rect(this.x, this.y, this.width, this.height);
+	pop();
+}
+
+
+function keyPressed() {
+  if (keyCode == LEFT_ARROW) {
+    direction = -1;
+  } else if (keyCode == RIGHT_ARROW) {
+    direction = 1;
+  }
+}
+
+function keyReleased() {
+	if (keyCode == LEFT_ARROW || keyCode == RIGHT_ARROW){
+		direction = 0;
+	}
+}
